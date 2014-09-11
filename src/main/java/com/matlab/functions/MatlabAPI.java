107938fb7cd50c38496c3199475bca2c924985c1
Dima <Dima>
2014-09-11 19:23:23 +0300

    init

---
+++ src/main/java/com/matlab/functions/MatlabAPI.java
@@ -0,0 +1,6 @@
+package com.matlab.functions;
+
+public interface MatlabAPI {
+
+	public void getFFT(String imagePath,int samplFrequency, int numberSpectrLines);
+}
---
+++ src/main/java/com/matlab/functions/MatlabDecorator.java
@@ -0,0 +1,18 @@
+package com.matlab.functions;
+
+/*
+ * http://www.javapractices.com/references/ReferencesAction.do;jsessionid=3874BFDEE6542D4B11574E03FBD344D7
+ */
+
+public class MatlabDecorator implements MatlabAPI{
+	
+	
+
+	public void getFFT(String imagePath, int samplFrequency,
+			int numberSpectrLines) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
