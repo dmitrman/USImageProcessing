b68bb5e9147b23c67441d58490bed92d3d67916f
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2014-11-14 10:13:47 +0200

    hom filt done

--- matlab/getHomomorphicFilter.m
+++ matlab/getHomomorphicFilter.m
@@ -16,4 +16,7 @@ If = fft2(I, M, N);
 Iout = real(ifft2(H.*If));
 Iout = Iout(1:size(I,1),1:size(I,2));
 Ihmf = exp(Iout) - 1;
+Ihmf=Ihmf-min(min(Ihmf));
+Ihmf=Ihmf/max(max(Ihmf));
+Ihmf=Ihmf.*255;
 end
--- src/main/java/com/presentation/MainScene.java
+++ src/main/java/com/presentation/MainScene.java
@@ -13,6 +13,7 @@ import javax.imageio.ImageIO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
 import com.matlab.functions.*;
 
 import javafx.application.Application;
@@ -24,15 +25,17 @@ import javafx.stage.Stage;
 public class MainScene extends Application {
 
 	public static void main(String[] args) {
-		//Application.launch(MainScene.class, (java.lang.String[]) null);
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");	
-		MatlabAPI a=(MatlabAPI) applicationContext.getBean("MatlabAPI");
-		Object[] res=null;
-		//res=a.getStatisticalFeatures("C:\\11.png", 20);
-		//res=a.readImage("C:\\11.png");
-		res=a.getHomomorphicFilter("C:\\11.png",3d);
-		System.out.println(res[0]);
-		
+		// Application.launch(MainScene.class, (java.lang.String[]) null);
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"ApplicationContext.xml");
+		MatlabAPI a = (MatlabAPI) applicationContext.getBean("MatlabAPI");
+		Object[] res = null;
+		// res=a.getStatisticalFeatures("C:\\11.png", 20);
+		// res=a.readImage("C:\\11.png");
+		res = a.getHomomorphicFilter("C:\\11.png", 10d);
+		MWNumericArray filt = (MWNumericArray) (res[0]);
+		// System.out.println(res[0]);
+		getImageFromMWNumericArray(filt);
 	}
 
 	// setenv('JAVA_HOME','C:\Program Files\Java\jdk1.6.0_32');
@@ -52,11 +55,49 @@ public class MainScene extends Application {
 		}
 	}
 	
-	public static Image getImageFromArray(int[] pixels, int width, int height) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        WritableRaster raster = (WritableRaster) image.getData();
-        raster.setPixels(0,0,width,height,pixels);
-        return image;
-    }
+	public static Image getImageFromMWNumericArray(MWNumericArray array) {
+		int[] dim = array.getDimensions();
 
+		int[] res = new int[dim[0] * dim[1]];
+		int s = 0;
+		for (int i = 0; i < dim[0] * dim[1]; i++) {
+			int g = array.getInt(i + 1);
+			res[i] = GetColorFromRGB(g, g, g);
+		}
+
+		writeImageToFile(getImageFromArray(res, dim[0], dim[1]), "png", "C:\\out.png");;
+		System.out.println(res[0] + " " + res[1] + " " + res[2]);
+		System.out.println(dim[0] * dim[1]);
+		return null;
+	}
+
+	private static int GetColorFromRGB(int r, int g, int b) {
+		int col = (r << 16) | (g << 8) | b;
+		return col;
+	}
+
+	public static BufferedImage getImageFromArray(int[] mas, int ih, int iw) {
+		BufferedImage image = new BufferedImage(iw, ih,
+				BufferedImage.TYPE_INT_RGB);
+		int s = 0;
+		for (int i = 0; i < ih; i++) {
+			for (int j = 0; j < iw; j++) {
+				int g = mas[s];
+				image.setRGB(i,j, GetColorFromRGB(g, g, g));
+				s++;
+
+			}
+		}
+		return image;
+	}
+
+	public static void writeImageToFile(BufferedImage im, String type,
+			String file) {
+		try {
+			File file1 = new File(file);
+			ImageIO.write(im, type, file1);
+		} catch (IOException ex) {
+
+		}
+	}
 }
